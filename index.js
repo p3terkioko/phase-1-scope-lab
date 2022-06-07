0eede5ec695f98f7fdc930ab934d866a39639fe6
Peter Mulu Kioko <peter.kioko@student.moringaschool.com>
2022-06-07 23:05:15 +0300

    commit tree 545787c826270b8d712e3ec978e40914024a95e2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+const leastFavoriteCustomer = 'Sam';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Samuel';
+}
+
+
